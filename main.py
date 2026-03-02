3a9e0fd8c4fe98479d4890b8924de6f7f5d0f52e
jhhnev336845110-web <jhhnev336845110@gmail.com>
2026-03-03 00:35:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ jobs:
         uses: Enteleform/buildozer-action@v1.2.1
         with:
           buildozer_version: master
-          command: android debug
-
+     
+command: android debug
     HEBREW_FONT = "hebrew.ttf"
 - name: Upload APK
         uses: actions/upload-artifact@v4
         with:
           name: carpool-app
           path: bin/*.apk
+
 
